6f6bfd6c3c4cdddff641f35e6bab9330d031ae8e
pedropatriota <pedro.p.patriota@gmail.com>
2023-06-11 20:58:33 +0100

    change git profile

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 import 'h8k-components';
 import Calculator from '././components/calculator/index.js';
 
-const title = "Calculator";
 
 function App() {
   return (
-    <div>
-      <h8k-navbar header={title} />
+    <div>    
       <Calculator/>
     </div>
   );
